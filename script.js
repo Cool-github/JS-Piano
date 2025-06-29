34bfe81e9e690e88dc61d15c4e3ba1e60951fb10
Gopi Chand <gopi.lucky8066@gmail.com>
2025-06-29 16:03:19 +0530

    .keyCode modified as it is depricated

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ const hints = document.querySelectorAll('.hints');
 
 // e is the event object
 function playSound(e){
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const keyCode = e.key.toUpperCase().charCodeAt(0); // Convert key to ASCII code (same as old keyCode)
+    const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
     // keyCode is a built-in property of the keyboard event object returns numerc code representing Alphabet 
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${keyCode}"]`);
 
     if(!key)return ; // if key isnt mapped ( pressing shift /  space )
     const keyNote = key.getAttribute("data-note");    // get musical note "C" "C#" 
